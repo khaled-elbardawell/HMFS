cf5994f49a691ee5c2fe725be6dd682b1ab87558
khaled-elbardawell <elbardawellkhaled@gmail.com>
2022-05-21 13:32:07 +0300

    make seeder for roles , users and permissions

--- app/Http/ViewComposers/MyViewComposer.php
+++ app/Http/ViewComposers/MyViewComposer.php
@@ -8,9 +8,8 @@ use Illuminate\Contracts\View\View;
 
 class MyViewComposer {
 
-
     public function compose(View $view) {
-        $user = UserOrganization::where('user_id',auth()->user()->id)->with(['organization','user.userRole.role.organization'])->first();
-        $view->with(['user'=>$user]);
+//        $user = UserOrganization::where('user_id',auth()->user()->id)->with(['organization','user.userRole.role.organization'])->first();
+//        $view->with(['user'=>$user]);
     }
 }
