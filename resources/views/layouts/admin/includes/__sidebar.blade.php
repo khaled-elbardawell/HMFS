54a9694ee8c2b1034575db16429ad164ec73d065
khaled-elbardawell <elbardawellkhaled@gmail.com>
2022-05-23 00:02:55 +0300

    merge

--- resources/views/layouts/admin/includes/__sidebar.blade.php
+++ resources/views/layouts/admin/includes/__sidebar.blade.php
@@ -7,10 +7,8 @@
                 <span>Home</span>
             </a>
         </li>
-<<<<<<< HEAD
-=======
 
->>>>>>> 7505adb956b901298f431403d228796ad3470747
+
         @can('role.index')
             <li>
                 <a class="nav-link" href="{{route('role.index')}}">
@@ -19,12 +17,8 @@
                 </a>
             </li>
         @endcan
-<<<<<<< HEAD
-        @can('organization.index')
-=======
 
         @can('is_super_admin')
->>>>>>> 7505adb956b901298f431403d228796ad3470747
             <li>
                 <a class="nav-link" href="{{route('organization.index')}}">
                     <i class="fa fa-sitemap"></i>
@@ -32,17 +26,9 @@
                 </a>
             </li>
         @endcan
-<<<<<<< HEAD
-        @can('users.index')
-        <li>
-            <a class="nav-link" href="{{route('users.index',['organization_id' => 1])}}">
-                <i class="fa fa-users"></i>
-                <span>Users</span>
-            </a>
-        </li>
-       @endcan
-       @can('task.index')
-=======
+
+
+
 
         @if(!Gate::allows('is_super_admin') && Gate::allows('users.index'))
                 <li>
@@ -55,7 +41,6 @@
 
 
         @can('task.index')
->>>>>>> 7505adb956b901298f431403d228796ad3470747
             <li>
                 <a class="nav-link" href="{{route('task.index')}}">
                     <i class="fa fa-tasks"></i>
