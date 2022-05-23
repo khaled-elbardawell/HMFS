f06b9cbd41a6c9042b39182bee28f8027fa11668
khaled-elbardawell <elbardawellkhaled@gmail.com>
2022-05-23 22:02:44 +0300

    builder html

--- Modules/Api/Routes/api.php
+++ Modules/Api/Routes/api.php
@@ -19,15 +19,16 @@ use Illuminate\Http\Request;
 
 
 Route::group([
-
     'middleware' => 'api',
     'prefix' => 'auth'
-
 ], function ($router) {
     Route::post('register', 'AuthController@register');
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
+
+
+
 
 });
